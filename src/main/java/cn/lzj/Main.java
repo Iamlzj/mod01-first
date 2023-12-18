5a3c610919e76acf63e15b159163419bcc96c0b0
lamlzj <2421070452@qq.com>
2023-12-19 01:06:38 +0800

    master commit2

--- src/main/java/cn/lzj/Main.java
+++ src/main/java/cn/lzj/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("hot-fix begin");
         System.out.println("Hello world!");
         System.out.println("hot-fix begin");
+        System.out.println("master begin");
     }
 }
